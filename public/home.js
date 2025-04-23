fd323e332c3124b65827332458b5f80c4481a9e1
Cashapp365 <fjmes@yahoo.com>
2025-04-23 15:20:38 +0100

    Update home.js

--- public/home.js
+++ public/home.js
@@ -117,7 +117,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <div class="placeholder-content">
             <i class="fas fa-home fa-3x"></i>
             <h3>Home Dashboard</h3>
-            <p>This is your personal dashboard. View your account information and settings here.</p>
+            <p>At Cash App, our mission is to redefine the world's relationship with money by making it more relatable, instantly available and universally accessible..</p>
           </div>
         `
         break
